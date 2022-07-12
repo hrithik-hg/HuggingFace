9f3f211e84c1c21ed52b99277087d13717bd9143
hrithik-hg <68283247+hrithik-hg@users.noreply.github.com>
2022-07-12 15:44:05 +0530

    Update single_inference.py

--- single_inference.py
+++ single_inference.py
@@ -11,4 +11,5 @@ model = T5ForConditionalGeneration.from_pretrained("t5-small")
 
 input_ids = tokenizer("translate English to German: The house is wonderful.", return_tensors="pt").input_ids
 outputs = model.generate(input_ids)
+print("English to German translation:- ", end="")
 print(tokenizer.decode(outputs[0], skip_special_tokens=True))
