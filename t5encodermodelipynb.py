bf15919e65853aadfc094c700a23d5befc6f850a
hrithik-hg <68283247+hrithik-hg@users.noreply.github.com>
2022-07-13 11:45:41 +0530

    Update t5encodermodelipynb.py

--- t5encodermodelipynb.py
+++ t5encodermodelipynb.py
@@ -11,24 +11,4 @@ input_ids = tokenizer(
 ).input_ids  # Batch size 1
 outputs = model(input_ids=input_ids)
 last_hidden_states = outputs.last_hidden_state
-
-# Here is an example of a device map on a machine with 4 GPUs using t5-3b, which has a total of 24 attention modules:
-model = T5ForConditionalGeneration.from_pretrained("t5-3b")
-device_map = {
-    0: [0, 1, 2],
-    1: [3, 4, 5, 6, 7, 8, 9],
-    2: [10, 11, 12, 13, 14, 15, 16],
-    3: [17, 18, 19, 20, 21, 22, 23],
-}
-model.parallelize(device_map)
-
-# On a 4 GPU machine with t5-3b:
-model = T5ForConditionalGeneration.from_pretrained("t5-3b")
-device_map = {
-    0: [0, 1, 2],
-    1: [3, 4, 5, 6, 7, 8, 9],
-    2: [10, 11, 12, 13, 14, 15, 16],
-    3: [17, 18, 19, 20, 21, 22, 23],
-}
-model.parallelize(device_map)  # Splits the model across several devices
-model.deparallelize()  # Put the model back on cpu and cleans memory by calling torch.cuda.empty_cache()
+print("The Output Hidden state:-", last_hidden_states)
